8ae096a0dee3abbca8fcdfe878a8595460e42687
Shawn <nhatquangta2911@gmail.com>
2022-02-11 15:19:37 +0700

    Tested change for Heroku

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        
+	<Route path="/test" element={<Home />} />	        
         </Routes>
       
         {/* <Footer /> */}
