d2f4ef05033c2e58cb421ab7c01a51ae16f3de51
QuangDung <dung05042000@gmail.com>
2022-02-16 22:58:04 +0700

    fixed deploy

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -10,7 +10,7 @@ function Home(){
         <>   <Navbar/>
             <Banner />
             <SliderSpeciality />
-           <PreviousNextMethods/>       
+              
            <SliderDoctor/>
            <Footer/>
 
