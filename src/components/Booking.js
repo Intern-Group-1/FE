834aabe9bf1a874315c98bcdebdd5ccbab2d0331
QuangDung <dung05042000@gmail.com>
2022-03-16 17:19:08 +0700

    bot

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
-
 import ApiCaller from '../utils/apiCaller';
-
 import Navbar from "./Navbar";
 import Footer from './Footer'
 import '../style/button.css'
