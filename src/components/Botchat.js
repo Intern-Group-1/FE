d5c6ed94e3f83490e27c4fa83b9c4c07d908a4fa
QuangDung <65674541+quangduxng@users.noreply.github.com>
2022-03-16 16:54:27 +0700

    Update Botchat.js

--- src/components/Botchat.js
+++ src/components/Botchat.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import React from 'react'
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
 
-function Botchat() {
-  return (
-    <MessengerCustomerChat
-    pageId="102460739014447"
-    appId="654919362449559"
-    //htmlRef="<REF_STRING>"
-  />
-  )
-}
+// function Botchat() {
+//   return (
+//     <MessengerCustomerChat
+//     pageId="102460739014447"
+//     appId="654919362449559"
+//     //htmlRef="<REF_STRING>"
+//   />
+//   )
+// }
 
-export default Botchat
+// export default Botchat
